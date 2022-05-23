08abbc8fffae6838fd2006d166903f3e81498cdb
f789520 <f789520@hotmail.com>
2022-05-23 18:25:33 +0800

    上線報告 2 email功能、更新簡介

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -38,11 +38,11 @@ export const About = (props) => {
           <div className="col-xs-12 col-md-6">
             <div className="about-text">
               <h2>關於我們</h2>
-              <p>{props.data ? props.data.paragraph : "loading..."}</p>
+              <p style={{fontSize:"30px" ,lineHeight:"38px"}}>{props.data ? props.data.paragraph : "loading..."}</p>
               <h3>為何選擇我們?</h3>
               <div className="list-style">
                 <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
+                  <ul  style={{fontSize:"20px"}}>
                     {props.data
                       ? props.data.Why.map((d, i) => (
                           <li key={`${d}-${i}`}>{d}</li>
@@ -51,7 +51,7 @@ export const About = (props) => {
                   </ul>
                 </div>
                 <div className="col-lg-6 col-sm-6 col-xs-12">
-                  <ul>
+                  <ul style={{fontSize:"20px"}}>
                     {props.data
                       ? props.data.Why2.map((d, i) => (
                           <li key={`${d}-${i}`}> {d}</li>
--- src/components/services.jsx
+++ src/components/services.jsx
@@ -33,20 +33,20 @@ export const Services = (props) => {
         <div className='container'>
           <div className='section-title'>
             <h2>服務內容</h2>
-            <p>
+            <p style={{fontSize:"20px" ,lineHeight:"38px"}}>
               法拍屋標購前中後，<br />會遇到各式各樣的法律問題，<br />
               法院的投標規則、<br />４種點交處理、<br />３５種不點交的處理、<br />產權剖析、產權安全過戶....<br />都會由我們專業法律團隊幫您把關
             </p>
             <br />
-            <p>點擊觀看影片:</p>
+            <p style={{fontWeight:"bold"}}>點擊觀看影片:</p>
          
-            <div>
-            <a href="https://youtu.be/o57-o9Wg22Q"> ＭＯＭＯＴＶ－今日大小事</a><br />
-            <a href="https://youtu.be/6CQ_xjVP5sc"> 【幸福空間直播】法拍屋投標流程與購屋技巧，法拍小哥來教你如何聰明買到超值便宜好房子！</a><br />
-            <a href="https://youtu.be/7fy9FsishgY"> 【幸福空間直播】法拍小哥，教你買便宜！如何判斷房價趨勢！</a><br />
-            <a href="https://www.youtube.com/watch?v=o57-o9Wg22Q"> 掌握這三招 5折買到房！20201001 Sway 【今天大小事】完整版</a><br />
-            <a href="https://www.youtube.com/watch?v=6CQ_xjVP5sc"> 【免百萬入手超值透天厝？ｘ 法拍屋入門必看ｘ 法拍屋投標流程與購屋技巧】【幸福空間直播】，法拍小哥來教你如何聰明買到超值便宜好房子！</a><br />
-            <a href="https://www.youtube.com/watch?v=7fy9FsishgY"> 【新手投資購屋三部曲ｘ 法拍屋入門必看ｘ如何判斷房價趨勢！】【幸福空間直播】法拍小哥，教你買便宜！</a><br />
+            <div style={{fontSize:"20px" ,lineHeight:"38px"}}>
+            <a href="https://youtu.be/o57-o9Wg22Q"> ．ＭＯＭＯＴＶ－今日大小事</a><br />
+            <a href="https://youtu.be/6CQ_xjVP5sc"> ．【幸福空間直播】法拍屋投標流程與購屋技巧，法拍小哥來教你如何聰明買到超值便宜好房子！</a><br />
+            <a href="https://youtu.be/7fy9FsishgY"> ．【幸福空間直播】法拍小哥，教你買便宜！如何判斷房價趨勢！</a><br />
+            <a href="https://www.youtube.com/watch?v=o57-o9Wg22Q"> ．掌握這三招 5折買到房！20201001 Sway 【今天大小事】完整版</a><br />
+            <a href="https://www.youtube.com/watch?v=6CQ_xjVP5sc"> ．【免百萬入手超值透天厝？ｘ 法拍屋入門必看ｘ 法拍屋投標流程與購屋技巧】【幸福空間直播】，法拍小哥來教你如何聰明買到超值便宜好房子！</a><br />
+            <a href="https://www.youtube.com/watch?v=7fy9FsishgY"> ．【新手投資購屋三部曲ｘ 法拍屋入門必看ｘ如何判斷房價趨勢！】【幸福空間直播】法拍小哥，教你買便宜！</a><br />
             
             </div>
           </div>
